ee6a7dd5d6aae4447e60007d811c01f4b70fea92
Arlan Dantas <arlanjose@gmail.com>
2022-05-30 17:17:12 -0300

    feat: StatusTicket typing

---
+++ StatusTicket.ts
@@ -0,0 +1,23 @@
+export enum STATUS_TICKET {
+    EXPECTING =  'expecting',
+    PROCESSED =  'processed',
+    CANCELED =  'canceled',
+}
+
+interface TicketStatus {
+    id: number;
+    validator_code: string;
+    device_name?: string;
+    state: STATUS_TICKET;
+    created_at: string | Date;
+}
+
+export interface Request {
+    dateId?: string | number;
+    lastId?: string | number;
+    batchSize?: number;
+}
+
+export interface Response {
+    [key: string]: TicketStatus;
+};
---
+++ index.ts
@@ -0,0 +1,1 @@
+export * as StatusTicket from "./StatusTicket";
