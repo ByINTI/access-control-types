f2a3b5cb81d74cbed35bf789466b057e88d1e05c
Arlan Dantas <arlanjose@gmail.com>
2022-06-20 14:52:49 -0300

    feat: EventDate expected audience

--- dist/models/EventDate.d.ts
+++ dist/models/EventDate.d.ts
@@ -4,6 +4,7 @@ export default interface EventDate {
     start_datetime: string;
     end_datetime: string;
     event_code: string;
+    expected_audience: number;
     updated_at: string | Date;
     created_at: string | Date;
 }
