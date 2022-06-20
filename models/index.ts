b6df0bf93657d3c1c5690d39e9f716fc855a65d4
Arlan Dantas <arlanjose@gmail.com>
2022-06-20 14:16:15 -0300

    feat: DateRelation model

--- models/index.ts
+++ models/index.ts
@@ -3,3 +3,4 @@ export { default as Event, EVENT_STATE } from "./Event";
 export { default as EventDate } from "./EventDate";
 export { default as Gate } from "./Gate";
 export { default as Ticket } from "./Ticket";
+export { default as DateRelation } from "./DateRelation";
