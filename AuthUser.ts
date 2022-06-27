2cdbabcbbcc2911c81d6a5ba7f7c91800ec734e4
Arlan Dantas <arlanjose@gmail.com>
2022-06-27 13:17:38 -0300

    feat: AuthUser

---
+++ AuthUser.ts
@@ -0,0 +1,10 @@
+export interface Request {
+    entityCode: string;
+    email: string;
+    password: string;
+}
+
+export interface Response {
+    name: string;
+    permissions: string[];
+}
---
+++ dist/AuthUser.d.ts
@@ -0,0 +1,9 @@
+export interface Request {
+    entityCode: string;
+    email: string;
+    password: string;
+}
+export interface Response {
+    name: string;
+    permissions: string[];
+}
