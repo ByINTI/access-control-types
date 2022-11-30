09d6d218b77675fbea2306ad16b728b9f54a19e9
Arlan Dantas <arlanjose@gmail.com>
2022-11-30 15:00:13 -0300

    feat: TicketRelation

---
+++ dist/models/TicketRelation.d.ts
@@ -0,0 +1,19 @@
+import EventDate from "./EventDate";
+import Price from "./Price";
+import Ticket from "./Ticket";
+import Event from "./Event";
+import Sector from "./Sector";
+interface TicketRelation {
+    ticket_id: number;
+    event_id: number;
+    date_id: number;
+    sector_id: number;
+    price_id: number;
+    created_at: string | Date;
+    ticket?: Ticket;
+    event?: Event;
+    date?: EventDate;
+    sector?: Sector;
+    price?: Price;
+}
+export default TicketRelation;
