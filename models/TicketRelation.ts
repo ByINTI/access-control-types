87cf2eaf86190b2fcdc2805597537e4d27c261f6
Arlan Dantas <arlanjose@gmail.com>
2022-12-02 08:59:34 -0300

    feat: TicketRelation id field

--- models/TicketRelation.ts
+++ models/TicketRelation.ts
@@ -5,17 +5,18 @@ import Event from "./Event";
 import Sector from "./Sector";
 
 interface TicketRelation {
+  id: number;
   ticket_id: number;
   event_id: number;
   date_id: number;
-  sector_id: number;
+  sector_id: number | null;
   price_id: number;
   created_at: string | Date;
 
   ticket?: Ticket;
   event?: Event;
   date?: EventDate;
-  sector?: Sector;
+  sector?: Sector | null;
   price?: Price;
 }
 
