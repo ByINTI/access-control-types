87d2ff3ae3e207cb22859b71ef788c8f0d5cc15a
Arlan Dantas <arlanjose@gmail.com>
2023-05-16 19:09:14 -0300

    Revert "Revert "Merge branch 'feature/12575/ticket_relations' into feature/12845/t4f_integration""
    This reverts commit ddc461d3327b78213cb933da43279a417e3f7023.

--- models/TicketStatus.ts
+++ models/TicketStatus.ts
@@ -1,4 +1,4 @@
-import { Ticket } from "./";
+import { Ticket, EventDate } from "./";
 
 export enum STATUS_TICKET {
     CREATED         = 'created',
@@ -17,9 +17,11 @@ export const TICKET_PROCESSED_STATES = [
 export default interface TicketStatus {
   id: number;
   ticket_id: number;
+  date_id: number | null;
   device_name?: string;
   state: STATUS_TICKET;
   created_at: string | Date;
 
   ticket?: Ticket;
+  date?: EventDate;
 }
