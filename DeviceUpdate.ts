25b368b12b00a471c2f321e12c81425b83f031b9
Arlan Dantas <arlanjose@gmail.com>
2023-07-24 15:25:01 -0300

    fix: Fix device type

--- DeviceUpdate.ts
+++ DeviceUpdate.ts
@@ -1,15 +1,11 @@
 import { IMSDevice } from "./AuthEntity";
 
 export interface Request {
-    entityCode: string;
     uuid: string;
     appVersion: string;
     type: string;
     name: string;
-    timestamp: number;
 }
 
-export type Response =  IMSDevice & {
-    entityCode: string,
-    entityName: string,
-};
+export type Response =  IMSDevice;
+
--- dist/DeviceUpdate.d.ts
+++ dist/DeviceUpdate.d.ts
@@ -1,13 +1,8 @@
 import { IMSDevice } from "./AuthEntity";
 export interface Request {
-    entityCode: string;
     uuid: string;
     appVersion: string;
     type: string;
     name: string;
-    timestamp: number;
 }
-export type Response = IMSDevice & {
-    entityCode: string;
-    entityName: string;
-};
+export type Response = IMSDevice;
