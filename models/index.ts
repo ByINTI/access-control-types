cc27267e39bb531e1f798f02ff6c3672a199a739
Arlan Dantas <arlanjose@gmail.com>
2025-11-26 00:33:14 -0300

    feat: Export ticketstates enum

--- models/index.ts
+++ models/index.ts
@@ -1,7 +1,7 @@
 export { default as TicketStatus, STATUS_TICKET, TICKET_PROCESSED_STATES } from "./TicketStatus";
 export { default as Event, EVENT_STATE } from "./Event";
 export { default as EventDate } from "./EventDate";
-export { default as Ticket } from "./Ticket";
+export { default as Ticket, TicketStates } from "./Ticket";
 export { default as DateRelation } from "./DateRelation";
 export { default as Price, TYPES as PRICE_TYPES } from "./Price";
 export { default as Sector } from "./Sector";
